5cb00ccefffcfec642b127b1b899a8b5ad64dc19
Anton Zakharov <ant.zaharov20177@yandex.ru>
2025-07-01 21:58:01 +0300

    создан модуль securityservice

---
+++ securityservice/src/main/java/ru/difembaxio/service/SecurityServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.difembaxio.service;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityServiceApplication.class,args);
+    }
+}
